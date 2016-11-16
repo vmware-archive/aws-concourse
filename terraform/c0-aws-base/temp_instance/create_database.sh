ca1e9ac3d8454845cf2c862f4ac44e95a30402d6
John Calabrese <jcalabrese@pivotal.io>
2016-11-15 20:32:25 -0500

    [#134394723] calling terraform requires us to first pull terraform

--- terraform/c0-aws-base/temp_instance/create_database.sh
+++ terraform/c0-aws-base/temp_instance/create_database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#sudo apt-get update
-#sudo apt-get install -y unzip
-#wget https://releases.hashicorp.com/terraform/0.7.9/terraform_0.7.9_linux_amd64.zip
-#unzip terraform_0.7.9_linux_amd64.zip
+sudo apt-get update
+sudo apt-get install -y unzip
+wget -O terraform.zip https://releases.hashicorp.com/terraform/0.7.11/terraform_0.7.11_linux_amd64.zip
+unzip terraform.zip
 # if terraform.tfstate file is empty remove the file
 if [[ ! -s terraform.tfstate ]]; then
     echo "Removing the terraform.tfstate..."
