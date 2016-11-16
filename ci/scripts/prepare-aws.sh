b5ef1a6a3ded064da0015e62b4c51b42ab825899
Caleb Washburn <cwashburn@pivotal.io>
2016-11-16 12:49:17 -0800

    [#134394723] adding support to save terraform.state file in s3

--- ci/scripts/prepare-aws.sh
+++ ci/scripts/prepare-aws.sh
@@ -4,3 +4,4 @@ mv /opt/terraform/terraform /usr/local/bin
 cd aws-prepare-get/terraform/c0-aws-base/
 terraform plan
 terraform apply
+cp terraform.tfstate pcfawsops-terraform-state-put/terraform.tfstate
