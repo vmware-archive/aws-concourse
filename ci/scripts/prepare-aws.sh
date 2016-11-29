f4b3ddcd1c840f58f42b1fbfd100244ccda2e5e7
datianshi <dsz0111@gmail.com>
2016-11-29 12:36:16 -0500

    Check the empty terraform states file

--- ci/scripts/prepare-aws.sh
+++ ci/scripts/prepare-aws.sh
@@ -4,7 +4,9 @@ mv /opt/terraform/terraform /usr/local/bin
 CWD=$(pwd)
 cd aws-prepare-get/terraform/c0-aws-base/
 
-cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
+if [[ $(cat $CWD/pcfawsops-terraform-state-get/terraform.tfstate | wc -l) gt 0 ]]; then
+  cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
+fi
 terraform plan
 terraform apply
 cp terraform.tfstate $CWD/pcfawsops-terraform-state-put/terraform.tfstate
