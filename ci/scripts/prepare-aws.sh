af80abd776cfcfe46b1e12187dd35cacb533cf82
datianshi <dsz0111@gmail.com>
2016-11-29 12:24:56 -0500

    Add back the copy terraform state file

--- ci/scripts/prepare-aws.sh
+++ ci/scripts/prepare-aws.sh
@@ -3,10 +3,8 @@ set -ex
 mv /opt/terraform/terraform /usr/local/bin
 CWD=$(pwd)
 cd aws-prepare-get/terraform/c0-aws-base/
-if [ -f "$CWD/pcfawsops-terraform-state-get/terraform.tfstate" ]
-then
-  cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
-fi
+
+cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
 terraform plan
 terraform apply
 cp terraform.tfstate $CWD/pcfawsops-terraform-state-put/terraform.tfstate
