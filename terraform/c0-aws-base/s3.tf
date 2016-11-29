7369abfe1415e1d67573fce8a29fd33d08404650
datianshi <dsz0111@gmail.com>
2016-11-28 18:51:02 -0600

    Force delete the s3 bucket

--- terraform/c0-aws-base/s3.tf
+++ terraform/c0-aws-base/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "pcf-bosh" {
     bucket = "${var.environment}-pcf-bosh"
     acl = "private"
+    force_destroy= true
 
     tags {
         Name = "${var.environment}-pcf-bosh"
@@ -11,6 +12,7 @@ resource "aws_s3_bucket" "pcf-bosh" {
 resource "aws_s3_bucket" "pcf-buildpacks" {
     bucket = "${var.environment}-pcf-buildpacks"
     acl = "private"
+    force_destroy= true
 
     tags {
         Name = "${var.environment}-pcf-buildpacks"
@@ -21,6 +23,7 @@ resource "aws_s3_bucket" "pcf-buildpacks" {
 resource "aws_s3_bucket" "pcf-droplets" {
     bucket = "${var.environment}-pcf-droplets"
     acl = "private"
+    force_destroy= true
 
     tags {
         Name = "${var.environment}-pcf-droplets"
@@ -31,6 +34,7 @@ resource "aws_s3_bucket" "pcf-droplets" {
 resource "aws_s3_bucket" "pcf-packages" {
     bucket = "${var.environment}-pcf-packages"
     acl = "private"
+    force_destroy= true
 
     tags {
         Name = "${var.environment}-pcf-packages"
@@ -41,6 +45,7 @@ resource "aws_s3_bucket" "pcf-packages" {
 resource "aws_s3_bucket" "pcf-resources" {
     bucket = "${var.environment}-pcf-resources"
     acl = "private"
+    force_destroy= true
 
     tags {
         Name = "${var.environment}-pcf-resources"
