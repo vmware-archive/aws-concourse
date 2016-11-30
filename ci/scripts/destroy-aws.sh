a5aad9ee8ac8c8baf3ef4bae6641fb4d42583971
datianshi <dsz0111@gmail.com>
2016-11-30 13:44:31 -0500

    Fix the destroy AWS scripts to clean AWS instances

--- ci/scripts/destroy-aws.sh
+++ ci/scripts/destroy-aws.sh
@@ -5,8 +5,8 @@ CWD=$(pwd)
 cd aws-prepare-get/terraform/c0-aws-base/
 cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
 
-export AWS_ACCESS_KEY_ID=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
-export AWS_SECRET_ACCESS_KEY=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
+export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
+export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
 export AWS_DEFAULT_REGION=${TF_VAR_aws_region}
 export VPC_ID=`terraform state show aws_vpc.PcfVpc | grep ^id | awk '{print $3}'`
 
@@ -14,11 +14,11 @@ export VPC_ID=`terraform state show aws_vpc.PcfVpc | grep ^id | awk '{print $3}'
 pip install awscli
 
 instances=$(aws ec2 describe-instances --filters Name=vpc-id,Values=$VPC_ID --output=json | jq -r '.[] | .[] | .Instances | .[] | .InstanceId')
-if [[ "X$instances" -ne "X" ]]
+if [[ "X$instances" != "X" ]]
 then
   echo "instances: $instances will be deleted......"
   aws ec2 terminate-instances --instance-ids $instances
-  aws ec2 wait --instance-ids $instances
+  aws ec2 wait instance-terminated --instance-ids $instances
 fi
 
 #Destroy the plan
