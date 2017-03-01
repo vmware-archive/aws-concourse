6bc4eda8d0c4533c7856007810350c18060a84fc
Kris Hicks <khicks@pivotal.io>
2017-03-01 15:47:54 -0800

    Fix init.sh missing shebang line

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 if [ ! -f ci/pcfaws_terraform_params.yml ]; then
   echo "Creating ci/pcfaws_terraform_params.yml"
   cp ci/sample/pcfaws_terraform_params.yml ci/pcfaws_terraform_params.yml
