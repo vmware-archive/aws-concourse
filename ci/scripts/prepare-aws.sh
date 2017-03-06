cde21660e52d867e27b06e433a09f05a1377a58d
Kris Hicks <khicks@pivotal.io>
2017-03-06 09:27:36 -0800

    Remove get of terraform state in prepare-aws
    This state does not exist until this pipeline runs.

--- ci/scripts/prepare-aws.sh
+++ ci/scripts/prepare-aws.sh
@@ -4,9 +4,6 @@ cp /opt/terraform/terraform /usr/local/bin
 CWD=$(pwd)
 cd aws-concourse/terraform/c0-aws-base/
 
-if [[ $(cat $CWD/pcfawsops-terraform-state-get/terraform.tfstate | wc -l) -gt 0 ]]; then
-  cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
-fi
 terraform plan
 
 set +e
