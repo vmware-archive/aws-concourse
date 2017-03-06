74ce7925e3bf05ada5ecb1e8fe38be65c9a31e08
Kris Hicks <khicks@pivotal.io>
2017-03-06 09:42:38 -0800

    Revert "Remove get of terraform state in prepare-aws"
    Putting this back for the moment since a deploy failed and there is an
    issue with running `terraform destroy` from the task container; I get
    kicked out of it after about 10 seconds or so.
    
    This reverts commit cde21660e52d867e27b06e433a09f05a1377a58d.

--- ci/scripts/prepare-aws.sh
+++ ci/scripts/prepare-aws.sh
@@ -4,6 +4,9 @@ cp /opt/terraform/terraform /usr/local/bin
 CWD=$(pwd)
 cd aws-concourse/terraform/c0-aws-base/
 
+if [[ $(cat $CWD/pcfawsops-terraform-state-get/terraform.tfstate | wc -l) -gt 0 ]]; then
+  cp $CWD/pcfawsops-terraform-state-get/terraform.tfstate .
+fi
 terraform plan
 
 set +e
