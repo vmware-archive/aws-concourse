0313fb4cb37f3d0a8bd757be6dbc13621bf22f73
jtgammon <jtgammon@gmail.com>
2017-04-10 10:19:29 -0500

    added -i option to ignore warnings on apply changes

--- scripts/deploy-director.sh
+++ scripts/deploy-director.sh
@@ -10,7 +10,7 @@ echo "==========================================================================
 
 # Apply Changes in Opsman
 
-om-linux --target https://opsman.$pcf_ert_domain -k \
+om-linux --target https://opsman.$pcf_ert_domain -k -i \
        --username "$pcf_opsman_admin" \
        --password "$pcf_opsman_admin_passwd" \
   apply-changes
